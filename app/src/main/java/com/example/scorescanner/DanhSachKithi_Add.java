0000ddc790069c4ef82bb7828901725dc3b6e1c5
nguyenthiennhan0402 <96nguyenthiennhan@gmail.com>
2023-09-11 20:16:52 +0700

    task giang

--- app/src/main/java/com/example/scorescanner/DanhSachKithi_Add.java
+++ app/src/main/java/com/example/scorescanner/DanhSachKithi_Add.java
@@ -16,11 +16,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DanhSachKithi_Add extends AppCompatActivity {
-    private List<String> list;
-    Spinner spinner;
+    private List<String> list, list2;
+    Spinner spinner, spinkieukithi;
     EditText txttenkithi,txttenkithi2,txttenkithi3;
     ImageButton savebtn2;
     String loaiphieu = "";
+    String kieukithi = "";
     Intent myintent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,29 +31,52 @@ public class DanhSachKithi_Add extends AppCompatActivity {
         txttenkithi = findViewById(R.id.txttenkithi);
         txttenkithi2 = findViewById(R.id.txttenkithi2);
         txttenkithi3 = findViewById(R.id.txttenkithi3);
+        spinkieukithi = findViewById(R.id.spinkieukithi);
         savebtn2 = findViewById(R.id.savebtn2);
+        //loai phieu
         list = new ArrayList<>();
-        list.add("20");
-        list.add("40");
-        list.add("50");
-        list.add("60");
-        list.add("120");
+        list.add("20");list.add("40");list.add("50");list.add("60");list.add("120");
+
+        list2 = new ArrayList<>();
+        list2.add("TN");
+        list2.add("TN & TL");
+
         myintent = getIntent();
         String username = myintent.getStringExtra("username");
+        //set du lieu  cho spinner loai phieu
         ArrayAdapter spinnerAdapter = new ArrayAdapter<>(this,
                 androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list);
         spinner.setAdapter(spinnerAdapter);
+
+
+        //set du lieu  cho spinner loai phieu
+        ArrayAdapter spinnerAdapter2 = new ArrayAdapter<>(this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list2);
+        spinkieukithi.setAdapter(spinnerAdapter2);
+
         //Bắt sự kiện cho Spinner, khi chọn phần tử nào thì hiển thị lên Toast
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 //đối số postion là vị trí phần tử trong list Data
                  loaiphieu = list.get(position).toString();
-
             }
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
                 loaiphieu = list.get(0).toString();
+            }
+        });
+
+        spinkieukithi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //đối số postion là vị trí phần tử trong list Data
+                kieukithi = list2.get(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                loaiphieu = list2.get(0).toString();
             }
         });
         savebtn2.setOnClickListener(new View.OnClickListener() {
