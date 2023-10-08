0879f898e6c546c57c4780a4e74405b5b6715830
sum812 <sssum812@gmail.com>
2023-10-08 12:36:05 +0700

    sum sum hinh anh, cham diem, ...

--- app/src/main/java/com/example/scorescanner/madeoption2.java
+++ app/src/main/java/com/example/scorescanner/madeoption2.java
@@ -3,19 +3,28 @@ package com.example.scorescanner;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class madeoption2 extends AppCompatActivity {
+    static String makithi;
+    static String username;
 
+    public static String getMakithi() {
+        return makithi;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_madeoption2);
-
 
         findViewById(R.id.tn_btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,10 +40,16 @@ public class madeoption2 extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.camera_cham_bai_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(madeoption2.this, CameraRealTime.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                makithi = getIntent().getStringExtra("makithi");
+                username = getIntent().getStringExtra("username");
+                Intent intent = new Intent(madeoption2.this, CameraRealTime.class);
+                intent.putExtra("makithi", makithi);
+                intent.putExtra("username", username);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
             }
         });
     }
